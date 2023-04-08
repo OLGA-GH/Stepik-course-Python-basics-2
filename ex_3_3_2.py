8a5f6943775e6c8dc41002581eb832e215683530
Olga <ovk773@gmail.com>
2023-04-08 16:58:34 +0400

    Added ex 3.3.2

---
+++ ex_3_3_2.py
@@ -0,0 +1,28 @@
+import requests
+import re
+from urllib.parse import urlparse
+
+url = input()
+
+res = requests.get(url)
+url_list = re.findall(r'<a\s+href=[\'"]([^./][^\'"]*)[\'"]', res.text)
+
+domains_list = []
+domains_formatted = []
+
+for _ in url_list:
+    parsed_url = urlparse(_)
+    if parsed_url.netloc:
+        domains_list.append(parsed_url.netloc)
+    elif parsed_url.path:
+        domains_list.append(parsed_url.path)
+
+for _ in domains_list:
+    domain = _.split(":")[0]
+    if domain not in domains_formatted:
+        domains_formatted.append(domain)
+
+domains_formatted.sort()
+
+for _ in domains_formatted:
+    print(_)
